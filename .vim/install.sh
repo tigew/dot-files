0e81e954a120151f4bda723ab51b873b5a78a473
travis2861 <cheadge@gmail.com>
2015-10-07 22:53:00 -0600

    Modifying the install script

--- .vim/install.sh
+++ .vim/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "runtime vimrc" > ~/.vimrc && mkdir ~/.vim && cd ~/.vim && git clone https://github.com/travis2861/vim-starter.git . && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+cd ~/.vim && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && mvim +PlugInstall
