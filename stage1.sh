1280372027a47ba395f54a54c7041653d88cf05e
travis2861 <cheadge@gmail.com>
2015-10-09 04:24:10 -0600

    I like order

--- stage1.sh
+++ stage1.sh
@@ -11,8 +11,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew install caskroom/cask/brew-cask
 brew cask install alfred
-brew cask install atom
 brew cask install android-studio
+brew cask install atom
 brew cask install brackets
 brew cask install cakebrew
 brew cask install copy
@@ -22,30 +22,31 @@ brew cask install evernote
 brew cask install firefox
 brew cask install github-desktop
 brew cask install gitup
+brew cask intsall gog-galaxy
 brew cask install intellij-idea-ce
 brew cask install keepassx
 brew cask install libreoffice
 brew cask install macdown
 brew cask install macvim
-brew cask install silverlight
-brew cask install syncthing
 brew cask install private-internet-access
 brew cask install pycharm-ce
 brew cask install razer-synapse
+brew cask install silverlight
 brew cask install skype
 brew cask install smartgit
 brew cask install spectacle
 brew cask install spotify
 brew cask install steam
 brew cask install sublime-text
+brew cask install syncthing
 brew cask install the-unarchiver
 brew cask install thunderbird
 brew cask install tower
+brew cask install unified-remote
 brew cask install utorrent
 brew cask install visual-studio-code
 brew cask install vlc
 brew cask install xquartz
-brew cask intsall gog-galaxy
 
 # In between stuff
 defaults write org.vim.MacVim MMNativeFullScreen 0 # fix for macvim fullscreen
