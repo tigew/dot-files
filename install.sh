75beaab2cd067084c13ae6b5590a2c6874c531d6
travis2861 <cheadge@gmail.com>
2015-10-09 02:23:15 -0600

    Added skeleton for install script
    Not tested ##
    add after install script to compile what needs to be compiled

--- install.sh
+++ install.sh
@@ -1,3 +1,54 @@
 #!/bin/sh
+# brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew cask add if checks with tags so we can do
+# an all install or just a force update sections
+
+brew install caskroom/cask/brew-cask
+brew cask install alfred
+brew cask install atom
+brew cask install android-studio
+brew cask install brackets
+brew cask install cakebrew
+brew cask install copy
+brew cask install dash
+brew cask install emacs
+brew cask install evernote
+brew cask install firefox
+brew cask install github-desktop
+brew cask install gitup
+brew cask install intellij-idea-ce
+brew cask install keepassx
+brew cask install libreoffice
+brew cask install macdown
+brew cask install macvim
+brew cask install silverlight
+brew cask install syncthing
+brew cask install private-internet-access
+brew cask install pycharm-ce
+brew cask install razer-synapse
+brew cask install skype
+brew cask install smartgit
+brew cask install spectacle
+brew cask install spotify
+brew cask install steam
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install thunderbird
+brew cask install tower
+brew cask install utorrent
+brew cask install visual-studio-code
+brew cask install vlc
+
+# brew cask fonts
+brew tap caskroom/fonts
+brew cask install font-inconsolata-for-powerline
+brew cask install font-source-code-pro-for-powerline
+
+# zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Vim stuff, do this last
 cd ~/.vim && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && mvim +PlugInstall
