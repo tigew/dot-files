9e9220e64217ca67d128e9afa5348dbcdff13294
travis2861 <cheadge@gmail.com>
2016-02-01 15:57:42 -0800

    Added google-drive cask

--- stage1.sh
+++ stage1.sh
@@ -19,6 +19,7 @@ brew install thefuck --verbose
 brew install caskroom/cask/brew-cask
 
 brew cask install alfred --verbose
+brew cask install google-drive --verbose
 brew cask install android-studio --verbose
 brew cask install appcleaner --verbose
 brew cask install atom --verbose
